af1be57563c4f47962a7a53578279953f85a605c
atharva-30 <113660975+atharva-30@users.noreply.github.com>
2025-12-20 01:59:39 +0530

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -51,7 +51,7 @@ export const PORTFOLIO_REELS = [
     title: "Commercial Outlet Reels",
     videos: [
       "https://go.screenpal.com/player/cTlIXfnYO3k?autoplay=0&muted=1&loop=1&controls=1",
-      "https://go.screenpal.com/player/cTlIXfnYO3k?autoplay=0&muted=1&loop=1&controls=1",
+      "https://go.screenpal.com/player/cTlIbqnYOEp?autoplay=0&muted=1&loop=1&controls=1",
       "https://go.screenpal.com/player/cTlIXfnYO3k?autoplay=0&muted=1&loop=1&controls=1",
     ],
   },
