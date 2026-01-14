772025018cee9e7aba4c406896f332c40c746481
atharva-30 <113660975+atharva-30@users.noreply.github.com>
2026-01-15 00:34:13 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Services from './components/Services';
-import Editor_Notes from './components/Testimonials';
+import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
