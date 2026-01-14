a19daaec12a5003faa944598e50d0390a90260f4
atharva-30 <113660975+atharva-30@users.noreply.github.com>
2026-01-14 18:16:53 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,7 +27,8 @@ const Hero: React.FC = () => {
           </h1>
 
           <p className="text-neutral-400 text-lg md:text-xl leading-relaxed max-w-2xl mb-8 mx-auto lg:mx-0">
-            I am a Professional Reel Video Grapher Editor behind Joltik Reelography, specializing in cinematic, fast-paced reels for events, weddings, festivals, and commercial spaces. My reels are designed to capture energy, emotion, and rhythm delivering impactful visuals that perform across digital platforms.
+            I am a Professional Reel Video Grapher Editor behind Joltik Reelography, specializing in cinematic, fast-paced reels for events, weddings, festivals, and commercial spaces. 
+            My reels are designed to capture energy, emotion, and rhythm delivering impactful visuals that perform across digital platforms.
           </p>
 
           <div className="flex flex-col sm:flex-row gap-4 justify-center lg:justify-start">
